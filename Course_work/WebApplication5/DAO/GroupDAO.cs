9b43b57c7fb05d8f4f2de879a3d0de02400de539
Inferus42 <furbo92@gmail.com>
2017-11-30 17:00:52 +0400

    test2

---
+++ Course_work/WebApplication5/DAO/GroupDAO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication5.Models;
+
+
+namespace WebApplication4.DAO
+{
+    public class GroupDAO
+    {
+        private SeaPortEntities _entities = new SeaPortEntities();
+        public IEnumerable<Group> getAllGroups()
+        {
+            return (from c in _entities.Group
+                    select c);
+
+        }
+    }
+
+
+}
